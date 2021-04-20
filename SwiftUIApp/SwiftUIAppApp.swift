cdf5e2326a2e9dd50c6c5f1a7a19fecb9ac3b4c1
Regina Nadbielny <reginanadbielny@gmail.com>
2021-04-20 16:16:15 -0400

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Regina Nadbielny on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
